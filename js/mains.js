d6e3f417af7193ad68909a4c213d4a03b00ff7f7
mohamed gassama <bouyagassama@gmail.cpm>
2017-05-27 18:31:50 -0400

    initial commit

---
+++ js/mains.js
@@ -0,0 +1,18 @@
+console.log("I'm ready");
+
+
+
+function main() {
+  $('.skillset').hide();
+  $('.skillset').fadeIn(1000);
+  $('.projects').hide();
+  $('.projects-button').on('click',    function(){
+    $(this).next().slideToggle(400);
+    $(this).toggleClass('active');
+    $(this).text('Projects Viewed');
+  });
+ }
+
+$(document).ready(main);
+
+
